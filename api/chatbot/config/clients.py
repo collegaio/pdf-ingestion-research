c22a7eb63a723931fcd191034cb87790a2498175
Jeremy Herzog <jeremyaherzog@gmail.com>
2024-06-27 21:46:30 -0400

    make empty dataset file

--- api/chatbot/config/clients.py
+++ api/chatbot/config/clients.py
@@ -2,10 +2,11 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core.indices import VectorStoreIndex
 from llama_index.core.objects import ObjectIndex
 from llama_index.embeddings.openai import OpenAIEmbedding, OpenAIEmbeddingModelType
-
-from chatbot.adapters.pinecone import PineconeAdapter
 from llama_index.llms.cohere import Cohere
-from chatbot.adapters.datasets import load_datasets_file
+
+from chatbot.chat.models import CDSDataset
+from chatbot.adapters.pinecone import PineconeAdapter
+from chatbot.adapters.datasets import DatasetsFile, load_datasets_file
 from chatbot.adapters.cohere import CohereChatAdapter
 from chatbot.config.env import COHERE_API_KEY, OPENAI_API_KEY, PINECONE_API_KEY
 
@@ -19,7 +20,8 @@ pinecone_adapter = PineconeAdapter(api_key=PINECONE_API_KEY)
 
 
 # TODO: move datasets file over
-datasets = load_datasets_file("../datasets.json")
+# datasets = load_datasets_file("../datasets.json")
+datasets = DatasetsFile(cds_files=[])
 
 # cds_query_tool = create_cds_query_router(
 #     datasets=datasets.cds_files,
