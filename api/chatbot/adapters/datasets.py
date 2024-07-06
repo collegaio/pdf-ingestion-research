c1fe45aeb70a32196a86a6ae833e1fc9cec691da
Jeremy Herzog <jeremyaherzog@gmail.com>
2024-07-06 18:10:04 -0400

    just remove all session setting

--- api/chatbot/adapters/datasets.py
+++ api/chatbot/adapters/datasets.py
@@ -47,18 +47,18 @@ def load_dataset(
 
 
 def load_datasets(config: DatasetsConfig) -> List[Datapoint]:
-    session = botocore.session.get_session()
+    # session = botocore.session.get_session()
 
-    client = session.create_client(
-        "sts",
-        aws_access_key_id=env.S3_ACCESS_KEY_ID,
-        aws_secret_access_key=env.S3_SECRET_ACCESS_KEY,
-    )
+    # client = session.create_client(
+    #     "sts",
+    #     aws_access_key_id=env.S3_ACCESS_KEY_ID,
+    #     aws_secret_access_key=env.S3_SECRET_ACCESS_KEY,
+    # )
 
-    credentials = client.get_session_token()["Credentials"]
+    # credentials = client.get_session_token()["Credentials"]
     # print(env.S3_ACCESS_KEY_ID)
     # print(env.S3_SECRET_ACCESS_KEY)
-    fs = s3fs.S3FileSystem(token=credentials["SessionToken"])
+    fs = s3fs.S3FileSystem()
     all_datasets = []
 
     for key in config.datasets:
