84de95d40b8bfd809eca24a206f13112b22a3b1b
Jeremy Herzog <jeremyaherzog@gmail.com>
2024-07-07 00:09:02 -0400

    use serve

--- webapp2/frontend/src/config/env.ts
+++ webapp2/frontend/src/config/env.ts
@@ -1,4 +1,13 @@
 // NOTE: these must be specified at build time
+console.log(import.meta.env.VITE_SOME_KEY);
 console.log(process.env.VITE_APP_BACKEND_URL);
-export const BACKEND_URL =
-  process.env.VITE_APP_BACKEND_URL || "http://localhost:3000";
+
+const mode = import.meta.env.MODE;
+
+let BACKEND_URL = "http://localhost:3000";
+
+if (mode === "production") {
+  BACKEND_URL = "https://communist-loise-collega-b748a8e9.koyeb.app";
+}
+
+export { BACKEND_URL };
