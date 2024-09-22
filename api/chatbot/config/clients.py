1d4b31f4e60d540e3a8505cfdd92fda24bfb945c
Jeremy Herzog <jeremyaherzog@gmail.com>
2024-09-22 16:30:07 -0400

    add aws region

--- api/chatbot/config/clients.py
+++ api/chatbot/config/clients.py
@@ -30,6 +30,7 @@ bedrock_client = boto3.client(
     "bedrock-runtime",
     aws_access_key_id=env.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=env.AWS_SECRET_ACCESS_KEY,
+    region_name=env.AWS_REGION,
 )
 pinecone_client = Pinecone(api_key=PINECONE_API_KEY)
 
